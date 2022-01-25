b2caeee60d649a65c765a84ead3d3fa1447cd4d0
majestyjnr <aidoojuniorsolomon@gmail.com>
2022-01-25 13:24:29 -0800

    Commit Updates

--- src/class.ts
+++ src/class.ts
@@ -1,7 +1,25 @@
 // CLASS
 // Classes are used to define objects
 
-
-class Employee {
-
+// Implementing interfaces with classes.
+interface EmployeeInterface {
+    id: number
+    name: string;
+    register() : string
 }
+
+// CREATING A FUNCTION
+class Employee implements EmployeeInterface{
+    id: number          // protected, private & public can be applied to properties of a class. 
+    name: string
+    constructor(id: number, name: string){
+        this.id = id
+        this.name = name
+    }
+    register(){
+        return `${this.name} has been registered successfully.`
+    }
+}
+const staff1 = new Employee(1, 'Solomon Aidoo Junior')
+const staff4 = new Employee(2, 'Developer Majesty')
+
