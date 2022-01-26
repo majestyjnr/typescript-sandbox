3f2992254903d6eb43677773086be117ee5b5fb3
majestyjnr <aidoojuniorsolomon@gmail.com>
2022-01-26 12:31:35 -0800

    Commit Updates

--- src/class.ts
+++ src/class.ts
@@ -23,3 +23,20 @@ class Employee implements EmployeeInterface{
 const staff1 = new Employee(1, 'Solomon Aidoo Junior')
 const staff4 = new Employee(2, 'Developer Majesty')
 
+
+// Extending a new class to the Employee Class. [ NB: Employee Class is an existing class ]
+class Person extends Employee{
+    position: string
+
+    constructor(id: number, name: string, position: string){
+        super(id, name)
+        this.position = position
+    }
+}
+
+const staff5 = new Person(3, 'Solomon Aidoo Jnr', 'Software Engineer')
+
+console.log(staff5.position); console.log(staff5.name); console.log(staff5.register); 
+
+
+
